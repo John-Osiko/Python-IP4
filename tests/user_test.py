11486e7a166e423877838fc426886b9826609256
John-Osiko <johnmaxosiko@gmail.com>
2020-09-29 14:31:06 +0300

    added test cases in files

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,7 +7,7 @@ class UserTest(unittest.TestCase):
         self.new_user = User(username='johnny', email = "test@test.com", bio ='default bio',password='bravo')
 
     def test_password_setter(self):
-        self.assertTrue(self.new_user.hashed_password is ) 
+        self.assertTrue(self.new_user.hashed_password) 
 
     def test_no_access_password(self):
         with self.assertRaises(AttributeError):
