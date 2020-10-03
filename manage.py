7dd991a17026f173cc57a02ef384a2ac63a9b128
John-Osiko <johnmaxosiko@gmail.com>
2020-10-03 09:47:45 +0300

    Generalised configuration settings

--- manage.py
+++ manage.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python
 from flask_script import Manager,Server
-from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User
 from app import create_app,db
 
-app = create_app('development')
+app = create_app()
 
-manager = Manager(app)
-manager.add_command('server', Server(use_debugger=True))
-
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+manager =  Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+manager.add_command('server',Server(use_debugger=True))
 
 @manager.shell
 def make_shell_context():
